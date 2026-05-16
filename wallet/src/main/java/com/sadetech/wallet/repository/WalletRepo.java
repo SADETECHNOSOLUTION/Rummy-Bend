4ff75e997955ef3999a4d47c2276796ac8869fcd
SADETECHNOSOLUTION <62300406+SADETECHNOSOLUTION@users.noreply.github.com>
2026-05-16 17:13:06 +0530

    Delete wallet directory

--- wallet/src/main/java/com/sadetech/wallet/repository/WalletRepo.java
+++
@@ -1,9 +0,0 @@
-package com.sadetech.wallet.repository;
-
-import com.sadetech.wallet.model.Wallet;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WalletRepo extends MongoRepository<Wallet,String> {
-}
